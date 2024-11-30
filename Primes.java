117319b927771349c95f1788ffe88b1cd6760761
agambh <agam.haim@post.runi.ac.il>
2024-11-30 22:48:23 +0200

    done Primes

--- Primes.java
+++ Primes.java
@@ -1,19 +1,22 @@
 public class Primes {
     public static void main(String[] args) {
         int max = Integer.parseInt( args[0] );
+        int primes = 0 ; 
         boolean [] arr = new boolean[ max + 1 ];
         for( int i = 0 ; i < arr.length ; i++ ){
             arr[i] = true ;
         }
         for(int j = 2 ; j< arr.length; j++ ){
             if( arr[j] == true ){
-                System.out.println(j);
-                for(int g = (j+1); g < arr.length ; g++){
-                    if(g % j ==0){
+                System.out.println( j );
+                primes ++ ;
+                for( int g = ( j + 1 ) ; g < arr.length ; g++ ){
+                    if(g % j == 0){
                         arr[g] = false ;
                     }
                 }
             }
         }
+        System.out.println("There are " + primes +" primes between 2 and "+ max );
     }
 }
